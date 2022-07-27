287222846397f73d72afab926d33bb2ddeeb5d92
mburuanthony <mburuantony@outlook.com>
2022-07-27 15:01:33 +0300

    updated context

--- src/context/optionsContext.js
+++ src/context/optionsContext.js
@@ -1,6 +1,10 @@
 import { useState, createContext, useContext } from "react";
 
 const optionsContext = createContext({
+  country: {},
+  setCountry: () => {},
+  fetchAnotherCountry: false,
+  setFetchAnotherCountry: () => {},
   option1: "",
   option2: "",
   option3: "",
@@ -10,11 +14,17 @@ const optionsContext = createContext({
 });
 
 export const OptionsProvider = ({ children }) => {
+  const [country, setCountry] = useState({});
+  const [fetchAnotherCountry, setFetchAnotherCountry] = useState(false);
   const [option1, setOption1] = useState("");
   const [option2, setOption2] = useState("");
   const [option3, setOption3] = useState("");
 
   const value = {
+    country,
+    setCountry,
+    fetchAnotherCountry,
+    setFetchAnotherCountry,
     option1,
     option2,
     option3,
